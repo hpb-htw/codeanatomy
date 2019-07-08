284fd61326a3250f4c37be5588e92ab20825f206
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2019-07-09 00:17:58 +0200

    add more examples in documents

--- build.lua
+++ build.lua
@@ -4,6 +4,15 @@ bundle = ""
 
 -- Typeset configuration
 typesetexe = "lualatex"
+typesetopts = "-interaction=nonstopmode -recorder"
 typesetdemofiles = {    
-    "codeanatomy.example.tex"
+    "codeanatomy.example.tex",
+    "codeanatomy.lstlisting.tex"
 }
+
+-- Copy all files match pattern to build/doc
+-- therefore use \input{java-program} instead of \input{demo-code/java-program.tex}
+docfiles = {
+    "demo-code/*"
+}
+
